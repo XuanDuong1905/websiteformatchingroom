5df872ccaed00cfa03ef0361a49be7309f499afc
XuanDuong1905 <caoduong19052006@gmail.com>
2026-06-01 15:02:43 +0700

    Initial setup Next.js with Prisma and MySQL

---
+++ src/lib/prisma.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "ghep_tro_db",
+  connectionLimit: 5,
+});
+
+const globalForPrisma = globalThis;
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
